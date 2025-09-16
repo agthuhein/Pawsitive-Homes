9733874e9f74b3d2d784fdf8a25463b766d60b0f
Aung Thu Hein <agthuhein.mm@gmail.com>
2025-09-16 10:59:39 +0200

    Add dogs and cats card into Landing

--- server/client/src/components/layout/Landing.js
+++ server/client/src/components/layout/Landing.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Dogs from '../card/Dogs';
+import Cats from '../card/Cats';
+import Footer from './Footer';
+
 const Landing = () => {
   return (
-    <section className='banner'>
-      <div className='container'>
-        <div className='banner-img'>
-          <img src='home.jpg' alt='Pet adoption' />
+    <>
+      <section className='banner'>
+        <div className='container'>
+          <div className='banner-img'>
+            <img src='home.jpg' alt='Pet adoption' />
+          </div>
+          <div className='banner-text'>
+            <h2>Every Paw Deserves a Home.</h2>
+            <p>
+              Each year, over 100,000 pets go without homes. At Pawsitive Home,
+              you can make a difference—adopt today.
+            </p>
+          </div>
         </div>
-        <div className='banner-text'>
-          <h2>Every Paw Deserves a Home.</h2>
-          <p>
-            Each year, over 100,000 pets go without homes. At Pawsitive Home,
-            you can make a difference—adopt today.
-          </p>
-        </div>
-      </div>
-    </section>
+      </section>
+      <Dogs />
+      <Cats />
+      <Footer />
+    </>
   );
 };
+
 export default Landing;
